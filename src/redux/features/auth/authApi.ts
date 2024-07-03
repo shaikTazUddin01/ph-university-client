58388ab0b1fd4ff856803896ea00215d134823eb
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-07-03 22:53:54 +0600

    created auth api

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,15 @@
+import { baseApi } from "../../api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInFo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInFo,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
