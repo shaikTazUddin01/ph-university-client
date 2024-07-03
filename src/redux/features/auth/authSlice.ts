b5e5737e23587f24d6d8aaee92428830bb2eb907
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-07-03 22:34:20 +0600

    create authSlice

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type TInitialState = {
+  user: null | object;
+  token: null | string;
+};
+
+const initialState: TInitialState = {
+  user: null,
+  token: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      (state.user = user), (state.token = token);
+    },
+    logout: (state) => {
+      (state.user = null), (state.token = null);
+    },
+  },
+});
+
+export const {setUser,logout}=authSlice.actions
+
+export default authSlice.reducer
