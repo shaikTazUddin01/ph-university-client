c97070cd477b3ecbda215a7fd4d0e7fa65222654
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-07-04 18:55:48 +0600

    added redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/router.tsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/features/store.tsx";
+import { persistor, store } from "./redux/features/store.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/features/store.tsx
+++ src/redux/features/store.tsx
@@ -1,16 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import { baseApi } from "../api/baseApi";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
