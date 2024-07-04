bd3bbbdf26657c91f90a71e287707a399d52f697
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-07-04 10:09:37 +0600

    fatch login info

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,28 +1,52 @@
-import { Button } from 'antd';
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import { Button } from "antd";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utiles/verifyToken";
+// import Password from "antd/es/input/Password";
 
 const Login = () => {
-    const {register,handleSubmit}=useForm()
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0002",
+      password: "admin123",
+    },
+  });
+  const [login, { data, error }] = useLoginMutation();
+  const dispatch = useDispatch();
 
-    const onSubmit=(data)=>{
-        console.log(data);
-    }
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-
-
-            <div>
-                <label htmlFor="id">Id:</label>
-                <input type="text" id="id" {...register('id')} />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input type="text" id="password" {...register('password')} />
-            </div>
-            <Button htmlType='submit'>submit</Button>
-        </form>
+  const onSubmit = async (data: Record<string, string>) => {
+    const userInFo = {
+      id: data.id,
+      password: data.password,
+    };
+    // console.log(userInFo);
+    const res = await login(userInFo).unwrap();
+    // const decoded=verifyToken(res.data.accessToken)
+    // console.log(decoded);
+    dispatch(
+      setUser({
+        user: "",
+        token: res.data.accessToken,
+      })
     );
+    console.log(res);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">Id:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">submit</Button>
+    </form>
+  );
 };
 
 export default Login;
--- src/redux/features/store.tsx
+++ src/redux/features/store.tsx
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import { baseApi } from "../api/baseApi";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
