57a042734d076faf57b361d4956efddfaadc2a72
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-08-21 22:37:27 +0600

    modify mycourse object

--- src/pages/Student/MyCourses.tsx
+++ src/pages/Student/MyCourses.tsx
@@ -1,17 +1,30 @@
+import { object } from "zod";
 import { useMyOfferCourseQuery } from "../../redux/features/enrollcourseApi";
 
 const MyCourses = () => {
-    const {data,isLoading}=useMyOfferCourseQuery(undefined)
+  const { data: offeredCourse, isLoading } = useMyOfferCourseQuery(undefined);
 
-    if(isLoading){
-        return <p>Loading..</p>
-    }
-    console.log(data);
-    return (
-        <div>
-            <h1>my courses</h1>
-        </div>
-    );
+  if (isLoading) {
+    return <p>Loading..</p>;
+  }
+
+  const singleObject = offeredCourse?.data?.reduce((acc, item) => {
+    const key = item.course.title;
+
+    acc[key] =acc[key] || { courseTitle: key,sections:[]};
+
+    acc[key].sections.push({section:item.section,_id:item._id})
+
+    return acc;
+  }, {});
+console.log(offeredCourse);
+  console.log(Object.values(singleObject?singleObject:{}));
+
+  return (
+    <div>
+      <h1>my courses</h1>
+    </div>
+  );
 };
 
 export default MyCourses;
